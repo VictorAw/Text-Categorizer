4cf43bb84e2c6997e2b99d2742fd41f9055001e4
VictorAw <VictorAw4@gmail.com>
2016-11-22 17:30:18 -0800

    Create skeleton for text analysis

--- app/helpers/text_analyzer.rb
+++ app/helpers/text_analyzer.rb
@@ -1,4 +1,4 @@
-require "net/http"
+require "algorithmia"
 
 class TextAnalyzer
   attr_reader :statistics
@@ -9,7 +9,7 @@ class TextAnalyzer
       titles: 0,
       pronouns: 0,
       names: 0,
-      quotation_marks: 0,
+      quotes: 0,
       adverbs: 0,
       adjectives: 0
     }
@@ -19,16 +19,26 @@ class TextAnalyzer
     reset_statistics
     
     @parts_of_speech = get_parts_of_speech(text)
-    @tokens = text.split
-    @tokens.each do |token, i|
-      
-    end
+    # Add to statistics here
 
+    # Divide each statistic with the total number of words in the text
+    @statistics.vals.each_with_index do |val, i|
+      @statistics[@statistics.keys[i]] = val.to_f / tokens.to_f
+    end
 
     @statistics
   end
 
   def get_parts_of_speech(text)
-    client = Algorithmia.client(
+    client = Algorithmia.client(Figaro.env.algorithmia_testing_key)
+    algorithm = client.algo("StanfordNLP/POSTagger/0.1.0")
+    algorithm.pipe(text).result
   end
+end
+
+class String
+  def is_num?
+    true if Float self resuce false
+  end
+end
 end
