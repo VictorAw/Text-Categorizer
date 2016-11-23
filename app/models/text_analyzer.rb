26821d038e9e353626116df567d0f1db2103ab00
apple <apple@apples-MacBook-Pro.local>
2016-11-23 13:34:23 -0800

    figaro setup

--- app/models/text_analyzer.rb
+++ app/models/text_analyzer.rb
@@ -1,3 +1,5 @@
+#Moved into models to make testing easier. Was in helper earlier
+
 require "algorithmia"
 
 class TextAnalyzer
@@ -17,10 +19,10 @@ class TextAnalyzer
 
   def analyze_text(text)
     reset_statistics
-    
+
     @parts_of_speech = get_parts_of_speech(text)
     # Add to statistics here
-
+    debugger
     # Divide each statistic with the total number of words in the text
     @statistics.vals.each_with_index do |val, i|
       @statistics[@statistics.keys[i]] = val.to_f / tokens.to_f
@@ -31,14 +33,26 @@ class TextAnalyzer
 
   def get_parts_of_speech(text)
     client = Algorithmia.client(Figaro.env.algorithmia_testing_key)
+    debugger
     algorithm = client.algo("StanfordNLP/POSTagger/0.1.0")
     algorithm.pipe(text).result
+  end
+
+  def reset_statistics
+    @statistics = {
+      numbers: 0,
+      titles: 0,
+      pronouns: 0,
+      names: 0,
+      quotes: 0,
+      adverbs: 0,
+      adjectives: 0
+    }
   end
 end
 
 class String
   def is_num?
-    true if Float self resuce false
+    true if Float self rescue false
   end
-end
 end
