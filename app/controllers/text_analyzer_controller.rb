324148e3c8f2a2dfd5448296a9b23e4f94baba66
apple <shaurya157@gmail.com>
2016-11-28 21:42:16 -0800

    frontend skeleton setup

---
+++ app/controllers/text_analyzer_controller.rb
@@ -0,0 +1,5 @@
+class TextAnalyzerController < ApplicationController
+  def index
+    render :index
+  end
+end
---
+++ spec/controllers/text_analyzer_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TextAnalyzerController, type: :controller do
+
+end
